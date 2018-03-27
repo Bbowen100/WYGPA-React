6d7c58257936975679486c3aed69bcbdf387d165
Brandon Bowen <brandonklbowen@gmail.com>
2018-03-26 23:17:59 -0400

    put config back in git ignore

--- models/comp-course.js
+++ models/comp-course.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 let compCourseSchema = new mongoose.Schema({
-  user_id: String,
-	courses: [ {name: String, weight: Number, mark: Number} ]
+  user_id: mongoose.Schema.Types.ObjectId,
+  courses: [{ name: String, weight: Number, mark: Number }]
 });
 
 module.exports = mongoose.model('compCourse', compCourseSchema);
