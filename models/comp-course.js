39a6b3655b03b25c696875fb2dd1bd25bb8f6ce6
Brandon Bowen <brandonklbowen@gmail.com>
2018-03-27 01:19:06 -0400

    changed user_id type to String

--- models/comp-course.js
+++ models/comp-course.js
@@ -1,8 +1,14 @@
 var mongoose = require('mongoose');
 
 let compCourseSchema = new mongoose.Schema({
-  user_id: mongoose.Schema.Types.ObjectId,
-  courses: [{ name: String, weight: Number, mark: Number }]
+  user_id: String,
+  courses: [
+    {
+      name: String,
+      weight: Number,
+      mark: Number
+    }
+  ]
 });
 
 module.exports = mongoose.model('compCourse', compCourseSchema);
--- models/curr-course.js
+++ models/curr-course.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
 let currCourseSchema = new mongoose.Schema({
-  user_id: mongoose.Schema.Types.ObjectId,
-  courses: [ {name:String, weight:Number, evals:[ {name: String, weight:Number, mark:Number}]} ],
+  user_id: String,
+  courses: [
+    {
+      name: String,
+      weight: Number,
+      evals: [{ name: String, weight: Number, mark: Number }]
+    }
+  ]
 });
 
 module.exports = mongoose.model('currCourse', currCourseSchema);
