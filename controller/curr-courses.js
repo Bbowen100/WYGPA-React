6855ccb37510ff2e5948f96d152e59508bf16ce0
Brandon Bowen <brandonklbowen@gmail.com>
2018-05-02 10:47:35 -0400

    FIX currcourse returns null for init add

--- controller/curr-courses.js
+++ controller/curr-courses.js
@@ -1,6 +1,75 @@
 const currentCourse = require('../models/curr-course');
 const mongoose = require('mongoose');
 
+//send the user's list of current courses
+module.exports.receive = function(req, res, next) {
+  let user_id = req.user.id;
+  currentCourse.findOne({ user_id }, (err, result) => {
+    if (err) {
+      return res.status(300).json(err);
+    } else if (!result) {
+      return res.status(202).json(result);
+    } else {
+      return res.status(200).json(result);
+    }
+    next();
+  });
+};
+//append a new course to a user's list of current courses
+module.exports.create = function(req, res, next) {
+  let course = req.body.currCourse;
+  let user_id = req.user.id;
+  currentCourse.findOne({ user_id }, (err, result) => {
+    if (err) return res.status(404).json(err);
+    else if (result) {
+      let courses = result.courses;
+      courses.push(course);
+      result.set({ courses });
+      result.save((err, updatedCourse) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json(updatedCourse);
+        next();
+      });
+    } else if (!result) {
+      var newCourse = {
+        user_id: mongoose.Types.ObjectId(user_id),
+        courses: [course]
+      };
+      currentCourse.findOneAndUpdate(
+        { user_id },
+        newCourse,
+        { new: true, upsert: true },
+        (err, n_Course) => {
+          if (err) return res.status(500).json(err);
+          return res.status(200).json(n_Course);
+          next();
+        }
+      );
+    }
+  });
+};
+// delete a specific course from ther's list of courses
+module.exports.delete = function(req, res, next) {
+  let user_id = req.user.id;
+  let { course_id } = req.query;
+  currentCourse.findOne({ user_id }, 'courses', (err, result) => {
+    if (err) return res.status(500).json(err);
+    let courses = result.courses;
+    let c_index = courses.findIndex(course => course._id == course_id);
+    if (c_index < 0) {
+      return res.status(400).json({ message: 'Course Not Found' });
+      next();
+    }
+    courses.splice(c_index, 1);
+    result.set({ courses });
+    result.save((err, updatedCourses) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(updatedCourses);
+      next();
+    });
+  });
+};
+
 // add an evalutation for a specific course
 module.exports.addEval = function(req, res, next) {
   let { course_id, eval } = req.body;
@@ -54,74 +123,6 @@ module.exports.delEval = function(req, res, next) {
     result.save((err, updateEvals) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json(updateEvals);
-      next();
-    });
-  });
-};
-//send the user's list of current courses
-module.exports.receive = function(req, res, next) {
-  let user_id = req.user.id;
-  currentCourse.findOne({ user_id }, (err, result) => {
-    if (err) {
-      return res.status(300).json(err);
-    } else if (!result) {
-      return res.status(202).json(result);
-    } else {
-      return res.status(200).json(result);
-    }
-    next();
-  });
-};
-//append a new course to a user's list of current courses
-module.exports.create = function(req, res, next) {
-  let course = req.body.currCourse;
-  let user_id = req.user.id;
-  currentCourse.findOne({ user_id }, (err, result) => {
-    if (err) return res.status(404).json(err);
-    else if (result) {
-      let courses = result.courses;
-      courses.push(course);
-      result.set({ courses });
-      result.save((err, updatedCourse) => {
-        if (err) return res.status(500).json(err);
-        return res.status(200).json(updatedCourse);
-        next();
-      });
-    } else if (!result) {
-      var newCourse = {
-        user_id: mongoose.Types.ObjectId(user_id),
-        courses: [course]
-      };
-      currentCourse.findOneAndUpdate(
-        { user_id },
-        newCourse,
-        { upsert: true },
-        (err, n_Course) => {
-          if (err) return res.status(500).json(err);
-          return res.status(200).json(n_Course);
-          next();
-        }
-      );
-    }
-  });
-};
-// delete a specific course from ther's list of courses
-module.exports.delete = function(req, res, next) {
-  let user_id = req.user.id;
-  let { course_id } = req.query;
-  currentCourse.findOne({ user_id }, 'courses', (err, result) => {
-    if (err) return res.status(500).json(err);
-    let courses = result.courses;
-    let c_index = courses.findIndex(course => course._id == course_id);
-    if (c_index < 0) {
-      return res.status(400).json({ message: 'Course Not Found' });
-      next();
-    }
-    courses.splice(c_index, 1);
-    result.set({ courses });
-    result.save((err, updatedCourses) => {
-      if (err) return res.status(500).json(err);
-      return res.status(200).json(updatedCourses);
       next();
     });
   });
