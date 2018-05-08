cc2f24d706f401db90dc039c8a859ee1d83b9de9
Brandon Bowen <brandonklbowen@gmail.com>
2018-05-08 17:13:50 -0400

    UPDATE converted callbacks to promises in middleware

--- middleware/index.js
+++ middleware/index.js
@@ -2,21 +2,26 @@ const userModel = require('../models/user');
 const jwt = require('jsonwebtoken');
 const env = require('../config/environment');
 
-module.exports.jwtAuthentication = (req, res, next)=>{
+module.exports.jwtAuthentication = (req, res, next) => {
   const bearerTokenHeader = req.header('Authorization');
   // If there is no Authorization header, it means this is an un-authed request.
   if (!bearerTokenHeader) {
-    return res.status(403).json({error: "No Authetication Header"});
+    return res.status(403).json({ error: 'No Authetication Header' });
   }
-  const token = bearerTokenHeader.split(" ")[1];
-  if(!token) return res.status(403).json({error: "No Token In Header"})
-  jwt.verify(token, env.JWT_SECRET, (err, decoded)=>{
-    userModel.findOne({_id: decoded.id}, (err, user) => {
-      if (err || !user) {
-        return res.status(403).json({error: "No Matching User For Token"});
-      }
-      req.user = {name: user.name, id: decoded.id};
-      next();
-    });
-  })
-}
+  const token = bearerTokenHeader.split(' ')[1];
+  if (!token) return res.status(403).json({ error: 'No Token In Header' });
+  jwt.verify(token, env.JWT_SECRET, (err, decoded) => {
+    userModel
+      .findOne({ _id: decoded.id })
+      .then(user => {
+        if (!user) {
+          return res.status(403).json({ error: 'No Matching User For Token' });
+        }
+        req.user = { name: user.name, id: decoded.id };
+        next();
+      })
+      .catch(err => {
+        return res.status(403).json({ error: err });
+      });
+  });
+};
