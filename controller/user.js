09d075e14f4758823f6e8eea8f2487bae14e2e8d
Brandon Bowen <brandonklbowen@gmail.com>
2018-05-08 17:00:25 -0400

    UPDATE converted callbacks to promises in user controller

--- controller/user.js
+++ controller/user.js
@@ -5,51 +5,64 @@ const env = require('../config/environment');
 
 module.exports.create = function(req, res, next) {
   let { user, password } = req.body;
-  userModel.findOne({ name: user }, (err0, result) => {
-    if (err0 || result) {
+  userModel
+    .findOne({ name: user })
+    .then(result => {
+      if (result) {
+        return res
+          .status(200)
+          .json({ bool: false, error: 'Username Already In Use' });
+      }
+      bcrypt.hash(password, null, null, function(err1, hash) {
+        // Store hash in your password DB.
+        if (err1) return res.status(500).json(err1);
+        userModel
+          .findOneAndUpdate(
+            { name: user },
+            { name: user, password: hash },
+            { upsert: true }
+          )
+          .then(result => {
+            return res.status(200).json({ bool: true, error: null });
+          })
+          .catch(err => {
+            return res.status(400).json(err2);
+          });
+      });
+    })
+    .catch(err => {
       return res
-        .status(200)
-        .json({ bool: false, error: 'Username Already In Use' });
-    }
-    bcrypt.hash(password, null, null, function(err1, hash) {
-      // Store hash in your password DB.
-      if (err1) return res.status(500).json(err1);
-      userModel.findOneAndUpdate(
-        { name: user },
-        { name: user, password: hash },
-        { upsert: true },
-        (err2, result) => {
-          if (err2) return res.status(400).json(err2);
-          return res.status(200).json({ bool: true, error: null });
-          next();
-        }
-      );
+        .status(403)
+        .json({ bool: false, error: 'Error Finding Username' });
     });
-  });
 };
 
 module.exports.receive = function(req, res, next) {
   let { user, password } = req.body;
-  userModel.findOne({ name: user }, (err0, result) => {
-    if (err0) return res.status(301).json({ bool: null, error: err0 });
-    else if (!result)
-      return res.status(202).json({ bool: false, error: 'No Such User' });
-    bcrypt.compare(password, result.password, (err1, boolean) => {
-      if (err1) {
-        return res.status(301).json({ bool: null, error: err1 });
-      } else if (boolean) {
-        const token = jwt.sign(
-          {
-            user: result.name,
-            id: result._id
-          },
-          env.JWT_SECRET
-        );
-        return res.status(200).json({ bool: boolean, error: null, token });
-      }
-      return res.status(200).json({ bool: boolean, error: null, token: null });
-      next();
+  userModel
+    .findOne({ name: user })
+    .then(result => {
+      if (!result)
+        return res.status(202).json({ bool: false, error: 'No Such User' });
+      bcrypt.compare(password, result.password, (err1, boolean) => {
+        if (err1) {
+          return res.status(301).json({ bool: null, error: err1 });
+        } else if (boolean) {
+          const token = jwt.sign(
+            {
+              user: result.name,
+              id: result._id
+            },
+            env.JWT_SECRET
+          );
+          return res.status(200).json({ bool: boolean, error: null, token });
+        }
+        return res
+          .status(200)
+          .json({ bool: boolean, error: null, token: null });
+      });
+    })
+    .catch(err => {
+      return res.status(301).json({ bool: null, error: err0 });
     });
-    next();
-  });
 };
